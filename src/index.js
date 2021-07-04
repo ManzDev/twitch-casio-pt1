118da9db2516926e0f8316f2e0d1f77e92788be3
ManzDev <JoseRomanHernandez@gmail.com>
2021-07-04 23:34:49 +0100

    Organize and fix long play keys

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import Soundfont from "https://cdn.skypack.dev/soundfont-player";
 import NOTES from "./assets/notesMapping.json";
 
 const ac = new AudioContext();
-const options = {
-  soundfont: "FluidR3_GM"
-};
+const options = { soundfont: "FluidR3_GM" };
 let currentInstrument = "piano";
 let currentVolume = 1;
 const cumbiaSong = new Audio("/cumbia.mp3");
+const playingNotes = {};
 
 const instruments = {
   piano: await Soundfont.instrument(ac, "acoustic_grand_piano", options),
@@ -16,8 +15,14 @@ const instruments = {
   flute: await Soundfont.instrument(ac, "flute", options)
 }
 
-const play = (instrument, note, duration = 2) => {
-  instruments[instrument].play(note, ac.currentTime, { duration, gain: currentVolume });
+const play = (instrument, note, duration = 6) => {
+  playingNotes[note] = instruments[instrument].start(note, ac.currentTime, { duration, gain: currentVolume });
+}
+
+const stop = (note) => {
+  const playingNote = playingNotes[note];
+  playingNote.stop();
+  playingNotes[note] = null;
 }
 
 const pianoKeys = document.querySelectorAll(".key");
@@ -25,21 +30,28 @@ const pianoKeys = document.querySelectorAll(".key");
 pianoKeys.forEach(key => {
   const note = key.dataset.note;
   key.addEventListener("mousedown", () => play(currentInstrument, note));
+  key.addEventListener("mouseleave", () => playingNotes[note] && stop(note));
+  key.addEventListener("mouseup", () => playingNotes[note] && stop(note));
 });
 
 document.addEventListener("keydown", (ev) => {
   const id = `key${ev.key}`;
   const note = NOTES[id];
-  const key = document.querySelector(`[data-note="${note}"]`);
-  key.classList.add("active");
-  play(currentInstrument, note);
+  if (!playingNotes[note] && note) {
+    const key = document.querySelector(`[data-note="${note}"]`);
+    key.classList.add("active");
+    play(currentInstrument, note);
+  }
 });
 
 document.addEventListener("keyup", (ev) => {
   const id = `key${ev.key}`;
   const note = NOTES[id];
-  const key = document.querySelector(`[data-note="${note}"]`);
-  key.classList.remove("active");
+  if (playingNotes[note] && note) {
+    const key = document.querySelector(`[data-note="${note}"]`);
+    key.classList.remove("active");
+    stop(note);
+  }
 });
 
 const toneSliders = document.querySelectorAll(".tone input");
